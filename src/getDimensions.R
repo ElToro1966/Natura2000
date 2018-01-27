b3c681f243140678b0059adc66d5bb4162229917
Eric Eikrem <eric@localhost.localdomain>
2018-01-27 12:02:21 +0100

    First commit with ProjectTemplate

--- src/getDimensions.R
+++ src/getDimensions.R
@@ -1,16 +1,6 @@
 # Natura 2000 R-project
 # Get dimensions of all tables for all years
 
-# Store the current directory
-initial.dir<-getwd()
-
-# Change to the new directory
-setwd("/media/eric/Disk2/Git/DataScience/Natura2000/Natura2000/")
-
-# Load the necessary libraries
-library(dplyr)
-library(readr)
-
 # Making tablenames from filenames
 tablenames_from_filenames <- function(v) {
   if (is.character(v)) {
@@ -25,7 +15,7 @@ tablenames_from_filenames <- function(v) {
 # Find file-name similar to table name
 filename_from_tablename <- function(name,year) {
   if (is.character(name)) {
-    name <- list.files(path = paste("data-original",year, sep = "/"), 
+    name <- list.files(path = paste("data",year, sep = "/"), 
                        pattern = paste0("^", name), ignore.case = TRUE)[1]
     return(name)
   }
@@ -37,8 +27,8 @@ dim_values <- function(t_year,t_filename) {
   out <- tryCatch(
     {
       message("Trying to read table and get dim-values")
-      dim(read.csv(file = paste("data-original",t_year,t_filename,sep = "/"), header=TRUE, sep=",",
-                   fileEncoding=toString(guess_encoding(paste("data-original",t_year,t_filename,sep = "/"))[1,1]),
+      dim(read.csv(file = paste("data",t_year,t_filename,sep = "/"), header=TRUE, sep=",",
+                   fileEncoding=toString(guess_encoding(paste("data",t_year,t_filename,sep = "/"))[1,1]),
                    dec=".")) 
       # The return value of `read.csv()` is the actual value 
       # that will be returned in case there is no condition 
@@ -48,32 +38,32 @@ dim_values <- function(t_year,t_filename) {
       # for the condition handlers for warnings and error below)
     },
     error=function(cond) {
-      message(paste("File does not seem to exist:", paste("data-original",t_year,t_filename,sep = "/")))
+      message(paste("File does not seem to exist:", paste("data",t_year,t_filename,sep = "/")))
       message("Here's the original error message:")
       message(cond)
       # Choose a return value in case of error
       return(c(NA,NA))
     },
     warning=function(cond) {
-      message(paste("URL caused a warning:", paste("data-original",t_year,t_filename,sep = "/")))
+      message(paste("URL caused a warning:", paste("data",t_year,t_filename,sep = "/")))
       message("Here's the original warning message:")
       message(cond)
       # Choose a return value in case of warning
       return(c(NA,NA))
     },
     finally={
-      message(paste("Processed URL:", paste("data-original",t_year,t_filename,sep = "/")))
+      message(paste("Processed URL:", paste("data",t_year,t_filename,sep = "/")))
     }
   )    
   return(out)
 }
 
 # Find folders
-data_folders <- list.files(path ="data-original", pattern = "^20*")
+data_folders <- list.files(path ="data", pattern = "^20*")
 
 # Find tables (from the frst folder)
 data_table_names <- tablenames_from_filenames(list.files(
-  path = paste("data-original", data_folders[1], sep = "/"), pattern = "*.csv"))
+  path = paste("data", data_folders[1], sep = "/"), pattern = "*.csv"))
 
 # Set table details
 data_table_dimensions = c("Nrow","Ncol")
@@ -94,10 +84,4 @@ for (year in data_folders) {
 }
 
 # Write to the output file
-write.table(data_table_dimensions, "summary_tables.txt", sep="\t") 
-
-# Unload the libraries
-detach("package:dplyr")
-
-# Change back to the original directory
-setwd(initial.dir)
+write.table(data_table_dimensions, "summary_tables.txt", sep="\t")
